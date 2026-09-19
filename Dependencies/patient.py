2a646cc3680018cc6989928b0affb15cd369afcb
Dwiti Thaker <138315448+DwitiThaker@users.noreply.github.com>
2026-09-19 17:59:42 +0530

    Rename function to get_patient_service

--- Dependencies/patient.py
+++ Dependencies/patient.py
@@ -2,6 +2,6 @@ from DB.mongodb import patient_collection
 from Repositories.patient_repository import PatientRepository
 from Services.patient_services import PatientService
 
-def get_patient_repository() -> PatientService:
+def get_patient_service() -> PatientService:
     repository = PatientRepository(patient_collection)
     return PatientService(repository)
